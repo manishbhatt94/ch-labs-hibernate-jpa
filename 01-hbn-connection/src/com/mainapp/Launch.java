4de4eaa7607563df433e67bb016bd12beb80c021
Manish Bhatt <manishbhatt594@gmail.com>
2026-06-04 17:26:50 +0530

    Get EntityManagerFactory reference object from Persistence static factory method

--- 01-hbn-connection/src/com/mainapp/Launch.java
+++ 01-hbn-connection/src/com/mainapp/Launch.java
@@ -1,9 +1,13 @@
 package com.mainapp;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Launch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hbn-connection");
 
 	}
 
