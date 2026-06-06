a4c05dbcf4b7b884886a139b1d6e7b31be6b72fb
Manish Bhatt <manishbhatt594@gmail.com>
2026-06-06 13:41:34 +0530

    Define connection details in persistence.xml & create EntityManager obj

--- 01-hbn-connection/src/com/mainapp/Launch.java
+++ 01-hbn-connection/src/com/mainapp/Launch.java
@@ -1,5 +1,6 @@
 package com.mainapp;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -7,7 +8,21 @@ public class Launch {
 
 	public static void main(String[] args) {
 
+		// Create EntityManagerFactory using the persistence unit name defined in
+		// persistence.xml configuration file:
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit-1");
+
+		// Create an EntityManager from the factory, which will be used to interact with
+		// the database:
+		EntityManager em = emf.createEntityManager();
+
+		System.out.println("EntityManager created successfully: " + em);
+
+		System.out.println("Connection to the database established successfully!");
+
+		// Close the EntityManager and EntityManagerFactory to release resources:
+		em.close();
+		emf.close();
 
 	}
 
